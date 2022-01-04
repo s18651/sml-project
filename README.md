8d7b41079904e7f0b8164dd54b2cbbf33deb7bd5
s18651 <s18651@pjwstk.edu.pl>
2022-01-04 10:52:47 +0100

    Add data downloader

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sml-project
 Implementation of World CO2 Emissions Analysis using Flask
+<br /><br />
+
+## How to start
+1. Go to Kaggle -> Account -> Generate API Key
+2. Place downloaded JSON into C:\Users\.kaggle
+3. Install kaggle <br />
+```pip install kaggle```
--- data.py
+++ data.py
@@ -1,14 +1,22 @@
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import numpy as np
-import pandas as pd
-from matplotlib.ticker import ScalarFormatter
-from matplotlib.ticker import FuncFormatter
-from textwrap import wrap
-
+import kaggle
 import os
-for dirname, _, filenames in os.walk('/data/raw'):
+
+dataset = 'manchunhui/world-development-indicators'
+raw_data_folder = './data/raw'
+number_of_raw_data_files = 0
+
+for dirname, _, filenames in os.walk(raw_data_folder):
     for filename in filenames:
         print(os.path.join(dirname, filename))
+        number_of_raw_data_files += 1
 
-df = pd.read_csv('/kaggle/input/world-development-indicators/WDIData_T.csv')
+# do not download files if they are already there
+if number_of_raw_data_files < 1:
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files(dataset, path=raw_data_folder, unzip=True)
+
+    # remove unnecessary files
+    os.remove(os.path.join(raw_data_folder, 'WDIData_T.csv'))
+    os.remove(os.path.join(raw_data_folder, 'WDIrevisions.xls'))
+
+
